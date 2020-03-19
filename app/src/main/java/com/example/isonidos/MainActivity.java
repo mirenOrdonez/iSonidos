c4ae14f4dfc5be87c22f1b76011d071ef677cbf1
Miren Ordóñez de Arce <miren.ordonez@gmail.com>
2020-03-19 19:01:47 +0100

    Con sonidos y videos funcionando correctamente

--- app/src/main/java/com/example/isonidos/MainActivity.java
+++ app/src/main/java/com/example/isonidos/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.isonidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void reproducirVideo (View vista) {
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        int idVideo = this.getResources().getIdentifier(vista.getTag().toString(), "raw", this.getPackageName());
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+idVideo);
+        videoView.setVideoURI(uri);
+        videoView.start();
     }
 }
